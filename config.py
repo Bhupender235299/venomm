7a522b324f328ce4e706afe2832c79ad5c4f7956
Bhupender235299 <bhupendersinghkantura@gmail.com>
2025-03-03 19:54:39 +0530

    Add files via upload

---
+++ config.py
@@ -0,0 +1,4 @@
+BOT_TOKEN = "7063499538:AAGIrgweIxMfm0BLIkIcyCsEsFkHspxwLJs"
+ADMIN_IDS = ["930577300"]
+# Default number of threads for attacks
+DEFAULT_THREADS = 1200  # Set the default number of threads to use for attacks
